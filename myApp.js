59389821895a957f2209f6cd3ab1287d6eec882b
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-09-17 12:30:36 +0300

    up

--- myApp.js
+++ myApp.js
@@ -1,60 +1,17 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const helmet = require("helmet");
 
 app.use(helmet());
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.use(helmet.hidePoweredBy());
 
 module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
+const api = require("./server.js");
+app.use(express.static("public"));
+app.disable("strict-transport-security");
+app.use("/_api", api);
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
