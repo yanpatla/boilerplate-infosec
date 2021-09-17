34461e168dfed7db2e139d78423174f74855f1a5
Tomas Leon Patlayan <tomipatlayan@gmail.com>
2021-09-17 12:55:28 +0300

    s

--- myApp.js
+++ myApp.js
@@ -13,6 +13,7 @@ app.use(express.static("public"));
 app.disable("strict-transport-security");
 app.use("/_api", api);
 app.get("/", function (request, response) {
+  helmet.xssFilter() 
   response.sendFile(__dirname + "/views/index.html");
 });
 let port = process.env.PORT || 3000;
